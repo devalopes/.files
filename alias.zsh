b74bee719291b3583e3a5beaec67ab6bf7439508
Alex Lopes <alexjonathanlopes@gmail.com>
2021-04-04 23:28:44 -0400

    Updated aliases

--- alias.zsh
+++ alias.zsh
@@ -6,3 +6,24 @@ alias myip='curl https://ifconfig.me'
 
 # Find & Delete all ".DS_Store" files (recursive)
 alias delete.ds="find . -name '.DS_Store' -type f -print -delete"
+
+# Git aliases
+alias gaa="git add -v --all"
+alias ga="git add"
+alias gf="git fetch -v"
+alias gc="git commit -v -m"
+alias gpush="git push -v"
+alias gpull="git pull -v"
+alias gs="git status"
+alias gcb="git checkout -b"
+alias gb="git branch"
+alias g="git"
+alias gcfg="git config --list"
+alias gcp="git cherry-pick"
+alias gd="git diff"
+alias gm="git merge"
+alias gl="git log"
+alias gr="git remote -v"
+alias gra="git remote add"
+alias gl="git log --decorate --graph --stat --all"
+

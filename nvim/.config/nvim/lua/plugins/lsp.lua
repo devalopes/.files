0176bbb01cf9c62433c250225df662053a65568e
Alex Lopes <alexjonathanlopes@gmail.com>
2022-08-16 22:37:15 -0400

    Go to definition split window

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -12,7 +12,7 @@ local lsp = require('lspconfig')
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
@@ -25,54 +25,55 @@ local on_attach = function(client, bufnr)
   -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Show line diagnostics automatically in hover window
-  --note: `vim.o.updatetime` setting is global and should be set only once
-  vim.o.updatetime = 100
-  vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
-  vim.api.nvim_create_autocmd("CursorHold", {
-  buffer = bufnr,
-  callback = function()
-    local opts = {
-      focusable = false,
-      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-      border = 'rounded',
-      source = 'always',
-      prefix = ' ',
-      scope = 'cursor',
-    }
-    vim.diagnostic.open_float(nil, opts)
-  end
-})
-
+  -- note: `vim.o.updatetime` setting is global and should be set only once
+  -- vim.o.updatetime = 100
+  -- vim.api.nvim_create_autocmd("CursorHold", {
+  --   buffer = bufnr,
+  --   callback = function()
+  --     local opts = {
+  --       focusable = false,
+  --       close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+  --       border = 'rounded',
+  --       -- source = 'always',
+  --       prefix = '',
+  --       scope = 'cursor', -- line
+  --     }
+  --     vim.diagnostic.open_float(nil, opts)
+  --   end
+  -- })
+  --
+  
+  --
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<leader>h', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+  -- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<leader>fmt', vim.lsp.buf.formatting, bufopts)
 end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-lsp.pyright.setup{
+lsp.pyright.setup {
   on_attach = on_attach,
   capabilities = capabilities
 }
-lsp.tsserver.setup{
+lsp.tsserver.setup {
   on_attach = on_attach,
   capabilities = capabilities
 }
-lsp["lua-language-server"].setup{
+lsp["sumneko_lua"].setup {
   on_attach = on_attach,
   capabilities = capabilities
 }
-require("rust-tools").setup{
+require("rust-tools").setup {
   server = {
     on_attach = on_attach,
     capabilities = capabilities
@@ -112,11 +113,19 @@ vim.diagnostic.config({
   severity_sort = false,
 })
 
+
+-- nil if current buffer
+local function bufwinid(bufnr)
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    if vim.api.nvim_win_get_buf(win) == bufnr then
+      return win
+    end
+  end
+end
+
 -- Go to definition split window
 local function goto_definition(split_cmd)
-  local util = vim.lsp.util
   local log = require("vim.lsp.log")
-  local api = vim.api
 
   local handler = function(_, result, ctx)
     if result == nil or vim.tbl_isempty(result) then
@@ -124,20 +133,25 @@ local function goto_definition(split_cmd)
       return nil
     end
 
-    if split_cmd then
-      vim.cmd(split_cmd)
-    end
-
     if vim.tbl_islist(result) then
-      util.jump_to_location(result[1], "utf-8", true)
-
       if #result > 1 then
-        util.set_qflist(util.locations_to_items(result))
-        api.nvim_command("copen")
-        api.nvim_command("wincmd p")
+        vim.fn.setqflist(vim.lsp.util.locations_to_items(result, "utf-8"))
+        vim.api.nvim_command("copen")
+      else
+        local uri = result[1].uri or result[1].targetUri
+        local current_win = bufwinid(vim.uri_to_bufnr(uri))
+        if (current_win == nil and split_cmd) then
+          vim.cmd(split_cmd)
+        end
+        vim.lsp.util.jump_to_location(result[1], "utf-8", { reuse_win = true })
       end
     else
-      util.jump_to_location(result, "utf-8", true)
+      local uri = result.uri or result.targetUri
+      local current_win = bufwinid(vim.uri_to_bufnr(uri))
+      if (current_win == nil and split_cmd) then
+        vim.cmd(split_cmd)
+      end
+      vim.lsp.util.jump_to_location(result, "utf-8", { reuse_win = true })
     end
   end
 

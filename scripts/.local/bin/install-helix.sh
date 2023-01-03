e7fb68ecfec99168ef3fd98ba1fa0ce6c5ee618d
Alex Lopes <alexjonathanlopes@gmail.com>
2023-01-02 19:59:57 -0500

    Helix install architecture independent

--- scripts/.local/bin/install-helix.sh
+++ scripts/.local/bin/install-helix.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+ARCHITECTURE=$(uname -m)
+
 if [ ! -x ~/.local/bin/hx ]; then
   mkdir -p ~/.local/bin
-  APPIMAGE=$(curl -s https://api.github.com/repos/helix-editor/helix/releases/latest | grep 'browser_download_url.*x86_64.AppImage' | grep -v '.zsync' | cut -d : -f 2,3 | tr -d \")
+  APPIMAGE=$(curl -s https://api.github.com/repos/helix-editor/helix/releases/latest | grep "browser_download_url.*$ARCHITECTURE.AppImage" | grep -v '.zsync' | cut -d : -f 2,3 | tr -d \")
   curl -L $APPIMAGE --output ~/.local/bin/hx
   chmod +x ~/.local/bin/hx
 else
